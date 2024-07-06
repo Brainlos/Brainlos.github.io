de3fd956df40757fd20f75ac9a67f6b4cfc94146
Brainlos <janakstack1@gmail.com>
2024-07-06 16:49:24 -0700

    Update Votes.js

--- Votes.js
+++ Votes.js
@@ -1,12 +1,12 @@
 localStorage.setItem('GojoVotes', '0');
 localStorage.setItem('SukunaVotes', '0');
 
-document.addEventListener('VotedGojo', function(event) {
+document.getElementById('GojoLink').addEventListener('click', function(VotedGojo) {
     let GojoPlus = GojoVotes + 1;
     localStorage.setItem('GojoVotes', GojoPlus.toString());
 });
 
-document.addEventListener('VotedSukuna', function(event) {
+document.addEventListener('VotedSukuna'.addEventListener('click', function(VotedSukuna) {
     let SukunaPlus = SukunaVotes + 1;
     localStorage.setItem('SukunaVotes', SukunaPlus.toString());
 });
