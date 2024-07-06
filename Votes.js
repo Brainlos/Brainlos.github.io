5393704707d4d6409cb0021c2b559d8a1cb3d712
Brainlos <janakstack1@gmail.com>
2024-07-06 16:25:11 -0700

    Update Votes.js

--- Votes.js
+++ Votes.js
@@ -1,9 +1,1 @@
-document.getElementById('GojoLink').addEventListener('click', function(event) {
-    const VotedGojo = new CustomEvent('linkClicked');
-    document.dispatchEvent(VotedGojo);
-});
 
-document.getElementById('SukunaLink').addEventListener('click', function(event) {
-    const VotedSukuna = new CustomEvent('linkClicked');
-    document.dispatchEvent(VotedSukuna);
-});
