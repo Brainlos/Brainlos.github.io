efd4a82e7b0d8351e8b73ca1149592d451dfcebd
Brainlos <janakstack1@gmail.com>
2024-07-06 16:49:55 -0700

    Update OnClick.js

--- OnClick.js
+++ OnClick.js
@@ -1,7 +1,1 @@
-document.getElementById('GojoLink').addEventListener('click', function(event) {
-    
-});
 
-document.getElementById('SukunaLink').addEventListener('click', function(event) {
-    
-});
