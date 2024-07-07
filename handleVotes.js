7e60fd7be40ec3c7b0c4066a30aca7ba2a518852
Brainlos <janakstack1@gmail.com>
2024-07-07 16:47:23 -0700

    Update handleVotes.js

--- handleVotes.js
+++ handleVotes.js
@@ -1,12 +1,1 @@
-document.addEventListener('voteGojo', function() {
-    let gojoVotes = parseInt(localStorage.getItem('GojoVotes'), 10);
-    gojoVotes += 1;
-    localStorage.setItem('GojoVotes', gojoVotes.toString());
-});
 
-// Handle Sukuna votes
-document.addEventListener('voteSukuna', function() {
-    let sukunaVotes = parseInt(localStorage.getItem('SukunaVotes'), 10);
-    sukunaVotes += 1;
-    localStorage.setItem('SukunaVotes', sukunaVotes.toString());
-});
