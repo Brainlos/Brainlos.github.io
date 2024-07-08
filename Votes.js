84715bf34990c8f7a38443563243559758bb2256
Brainlos <janakstack1@gmail.com>
2024-07-07 18:05:00 -0700

    Update Votes.js

--- Votes.js
+++ Votes.js
@@ -5,14 +5,22 @@ if (localStorage.getItem('SukunaVotes') === null) {
     localStorage.setItem('SukunaVotes', '0');
 }
 
-document.getElementById('GojoLink').addEventListener('click', function() {
+document.getElementById('GojoLink').addEventListener('click', function(event) {
+    event.preventDefault();
     let gojoVotes = parseInt(localStorage.getItem('GojoVotes'), 10);
     gojoVotes += 1;
     localStorage.setItem('GojoVotes', gojoVotes.toString());
+    updateLink('GojoLink', gojoVotes);
 });
 
-document.getElementById('SukunaLink').addEventListener('click', function() {
+document.getElementById('SukunaLink').addEventListener('click', function(event) {
+    event.preventDefault();
     let sukunaVotes = parseInt(localStorage.getItem('SukunaVotes'), 10);
     sukunaVotes += 1;
     localStorage.setItem('SukunaVotes', sukunaVotes.toString());
+    updateLink('SukunaLink', sukunaVotes);
 });
+
+function updateLink(linkId, votes) {
+    document.getElementById(linkId).href = `https://brainlos.github.io/${linkId.split('Link')[0]}/?${linkId.split('Link')[0]}VotesSend=${votes}`;
+}
